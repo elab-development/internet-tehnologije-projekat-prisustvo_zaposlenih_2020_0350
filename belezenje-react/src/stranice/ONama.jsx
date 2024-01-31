2350c62f5682879f486d5a6a1e242a9cce61cf5b
Nina Zivkovic <nina.zivko2808@gmail.com>
2024-01-31 19:31:03 +0100

    kreirana stranica ONama

--- belezenje-react/src/stranice/ONama.jsx
+++ belezenje-react/src/stranice/ONama.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Welcome from "../komponente/Welcome";
+import Footer from "../komponente/Footer";
+import nina from "../slike/nina.jpeg";
+import mina from "../slike/Mina.png";
+import {Row} from "react-bootstrap";
+import PocetnaKartica from "../komponente/PocetnaKartica";
 
 const ONama = () => {
+
+    const onama = [
+        {
+            id: 1,
+            naslov: "Mina Zavišić",
+            tekst: "Mina je studentkinja četvrte godine FONa, smer ISIT. Voli da programira i da se bavi sportom.",
+            slika: mina
+        },
+        {
+            id: 2,
+            naslov: "Nina Živković",
+            tekst: "Nina je studentkinja četvrte godine FONa, smer ISIT. Voli da isprobava novu hranu i čita modne časopise.",
+            slika: nina
+        }
+    ];
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Welcome poruka="Nešto" opis="o nama"/>
+                <Row className="m-3">
+                    {
+                        onama.map(kartica => {
+                            return (
+                                <div key={kartica.id} className="col-md-6">
+                                    <PocetnaKartica slika={kartica.slika} naslov={kartica.naslov} tekst={kartica.tekst}/>
+                                </div>
+                            );
+                        })
+                    }
+                </Row>
+            <Footer />
+        </>
     );
 };
 
