67eee17bfb03320bbdfc9b38f017ecdf4d763519
Nina Zivkovic <nina.zivko2808@gmail.com>
2024-02-06 19:27:34 +0100

    prikaz prisustva za dogadjaj

--- belezenje-react/src/stranice/Dogadjaji.jsx
+++ belezenje-react/src/stranice/Dogadjaji.jsx
@@ -4,10 +4,14 @@ import Footer from "../komponente/Footer";
 import Welcome from "../komponente/Welcome";
 import { Calendar, dayjsLocalizer } from 'react-big-calendar'
 import dayjs from 'dayjs'
+import {Col, Row} from "react-bootstrap";
+import axios from "axios";
 
 const Dogadjaji = () => {
 
     const [dogadjaji, setDogadjaji] = useState([]);
+    const [izabraniDogadjaj, setIzabraniDogadjaj] = useState({});
+    const [prisustva, setPrisustva] = useState([]);
 
     useEffect(() => {
         instanca.get("/dogadjaji").then(res => {
@@ -17,7 +21,7 @@ const Dogadjaji = () => {
             const podaciZaKalendar = podaci.map(podatak => {
 
                 return {
-                    id: podatak.id_dogadjaja,
+                    id: podatak.id,
                     title: podatak.naziv_dogadjaja,
                     start: new Date(podatak.vreme_pocetka),
                     end: new Date(podatak.vreme_kraja)
@@ -31,29 +35,74 @@ const Dogadjaji = () => {
         });
     }, []);
 
+    useEffect(() => {
+        if (izabraniDogadjaj.id !== undefined) {
+            instanca.get("/pretraga-po-dogadjaju?dogadjaj_id=" + izabraniDogadjaj.id).then(res => {
+                console.log(res);
+                let podaci = res.data.podaci;
+                setPrisustva(podaci);
+            }).catch(err => {
+                console.log(err);
+            });
+        } else {
+            console.log("Nema izabranog dogadjaja");
+        }
+    }, [izabraniDogadjaj]);
+
     const localizer = dayjsLocalizer(dayjs)
 
     return (
         <>
             <Welcome poruka="Dogadjaji" opis="u vasem kalendaru"/>
             <div>
-                <Calendar
-                    localizer={localizer}
-                    events={dogadjaji}
-                    startAccessor="start"
-                    endAccessor="end"
-                    titleAccessor="title"
-                    style={{ height: 500 }}
-                    onSelectEvent={event => {
-                        let text = `Naziv dogadjaja: ${event.title}\nVreme pocetka: ${event.start}\nVreme kraja: ${event.end}`;
-                        alert(text);
-                    }}
-                />
+                <Row className="mt-3">
+                   <Col md={9}>
+                       <Calendar
+                           localizer={localizer}
+                           events={dogadjaji}
+                           startAccessor="start"
+                           endAccessor="end"
+                           titleAccessor="title"
+                           style={{ height: 500 }}
+                           onSelectEvent={event => {
+                            let text = `Naziv dogadjaja: ${event.title}\nVreme pocetka: ${event.start}\nVreme kraja: ${event.end}`;
+                            setIzabraniDogadjaj(event);
+                            alert(text);
+                           }}
+                       />
+                   </Col>
+                    <Col md={3}>
+                        <h2>Prisustva za dogadjaj {izabraniDogadjaj ? izabraniDogadjaj.title : null }</h2>
+                        <ul>
+                            {
+                                prisustva && prisustva.map(p => {
+                                    return (
+                                        <li key={p.id}>
+                                            {p.user.name} - {p.ocena.opis}
+                                        </li>
+                                    );
+                                })
+                            }
+                        </ul>
+
+                    </Col>
+                </Row>
+
             </div>
 
-            <Footer />
+            <Row className="m-3">
+                <div className="col-md-12">
+                    <h2>Legenda</h2>
+                    <p>
+                        Klikom na dogadjaj u kalendaru možete videti više informacija.
+                    </p>
+                </div>
+
+            </Row>
+
+            <Footer/>
         </>
     );
 };
 
-export default Dogadjaji;
+export default Dogadjaji;
